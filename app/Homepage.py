cd05530452aa0364c4279e76c141c6b5a6cd3f92
Vivek Katial <vivekkatial@gmail.com>
2024-04-12 15:48:30 +1000

    feat: Adding func for ISA in app + new things on homepage

--- app/Homepage.py
+++ app/Homepage.py
@@ -11,12 +11,11 @@ st.sidebar.success("Select a page above.")
 
 st.markdown(
     """
-    This is a StreamLit app that showcases my PhD work.
+    This is a StreamLit app that showcases my PhD work on Quantum Approximate Optimization Algorithm (QAOA).
 
-    The app is divided into two sections:
-    - **Instance Space Analysis**: This section provides an overview of the instance space analysis for the problem.
-    - **Landscape**: This section provides an overview of the optimal angles for the problem.
-    
-    $$ f(x) = x^2 $$
-"""
+    The app is divided into three sections:
+    - **QAOA Landscape**: Here we visualize the QAOA landscape at different layers for various instance classes
+    - **QAOA Parameter Evolution**: On this page we visualize the evolution of QAOA parameters -- in particular we're interested in how the parameters concentrate around different modes.
+    - **Instance Space Analysis**: We provide an interface to conduct Instance Space Analysis for various experiments studying the QAOA algorithm
+    """
 )
--- app/pages/3_Instance_Space_Analysis.py
+++ app/pages/3_Instance_Space_Analysis.py
@@ -5,6 +5,7 @@ import time
 import numpy as np
 import pandas as pd
 import os
+import scipy.io
 
 st.set_page_config(page_title="Instance Space Analysis", page_icon="📈",     layout="wide",
     initial_sidebar_state="expanded",)
@@ -22,7 +23,7 @@ def csv_to_latex(csv_file_path):
     return latex_str
 
 
-def plot_facetted_data(d_coords, d_data, exclude_columns=None):
+def plot_facetted_data(d_coords, d_data, exclude_columns=None, binary_scale=False):
     """
     Creates a single facetted plot with multiple facets where each facet is a scatter plot of z_1 vs z_2,
     colored by the values of different data columns from d_data, with an option to exclude specific columns.
@@ -50,6 +51,14 @@ def plot_facetted_data(d_coords, d_data, exclude_columns=None):
     # Prepare the long-form DataFrame for facetting
     melted_df = merged_df.melt(id_vars=['Row', 'z_1', 'z_2'], var_name='DataColumn', value_name='Value')
 
+    # Calculate how many columns to wrap the facets
+    num_columns = len(melted_df['DataColumn'].unique())
+
+    # Update the color scale to be PER chart
+    colorscale = [(0, "#F8F71A"), (0.3, "#A1C840"), (0.6, "#1CAADF"), (0.9, "#4742E6"), (1, "#3F29B2")]
+    
+        
+
     # Create the facetted plot using Plotly Express
     fig = px.scatter(
         melted_df,
@@ -58,10 +67,11 @@ def plot_facetted_data(d_coords, d_data, exclude_columns=None):
         color='Value',
         facet_col='DataColumn',
         facet_col_wrap=3,  # Adjust the number of columns per row as necessary
-        color_continuous_scale='Viridis',
+        color_continuous_scale=[(0, "#F8F71A"), (0.3, "#A1C840"), (0.6, "#1CAADF"), (0.9, "#4742E6"), (1, "#3F29B2")],
         title='',
         labels={'Value': 'Data Value'},
-        height=1000,  # Adjust the height as necessary
+        # if more than 6 columns use smaller height
+        height=1600 if num_columns > 6 else 800,
     )
 
     # Update the layout if needed
@@ -70,15 +80,48 @@ def plot_facetted_data(d_coords, d_data, exclude_columns=None):
         hovermode='closest'
     )
     
-    # Update the axis titles for each facet
-    for axis in fig.layout:
-        if axis.startswith('xaxis') or axis.startswith('yaxis'):
-            fig.layout[axis].title.text = ''
-    
     # Update the facet titles to be more readable
     for annotation in fig.layout.annotations:
         feature_name = annotation.text.split('=')[1].replace('_', ' ')
+        # Add <b> to split the title into two lines
+        feature_name = f'<b>{feature_name}</b>'
         annotation.text = feature_name
+        # Make color of title black
+        annotation.font.color = 'black'
+    
+    # Update each facet to have its own colorscale
+    for i, trace in enumerate(fig.data):
+        trace.marker.coloraxis = f'coloraxis{i+1}'
+    
+    for fr in fig.frames:
+    # update each of the traces in each of the animation frames
+        for i, t in enumerate(fr.data):
+            t.update(coloraxis=f"coloraxis{i+1}")
+    
+    
+    coloraxis_config = {}
+    for i in range(1, num_columns + 1):
+        axis_name = f'coloraxis{i}'
+
+        # If only 2 values in the column, use a different color scale
+        if len(melted_df[melted_df['DataColumn'] == melted_df['DataColumn'].unique()[i - 1]]['Value'].unique()) == 2:
+            colorscale = [(0, "#F8F71A"), (1, "#3F29B2")]
+            if binary_scale:
+                colorscale = [(0, "orange"), (1, "blue")]
+        else:
+            colorscale = [(0, "#F8F71A"), (0.3, "#A1C840"), (0.6, "#1CAADF"), (0.9, "#4742E6"), (1, "#3F29B2")]
+
+        coloraxis_config[axis_name] = {
+            "colorbar": {"showticklabels": False},  # Hide colorbar tick labels
+            "showscale": False,  # Hide the colorbar itself
+            "colorscale": colorscale
+        }
+
+    # Apply the dynamic color axis configuration to the figure layout
+    fig.update_layout(**coloraxis_config)
+    # Apply simple theme
+    fig.update_layout(template="simple_white")
+
 
     return fig
 
@@ -153,9 +196,14 @@ experiment_dir = os.path.join(data_dir, experiment)
 
 # Read in the coordinates.csv file for the experiment 
 d_coords = pd.read_csv(os.path.join(experiment_dir, "coordinates.csv"))
-# Extract the source based on whats after the hash `742a761d5ca8400d86cd72e76811737d_three_regular_graph`
-d_coords['Source'] = d_coords['Row'].str.extract(r'_(\w+)$')
-d_coords['Source'] = d_coords['Source'].str.title().str.replace('_', ' ')
+# Read in the metadata file for the experiment
+d_metadata = pd.read_csv(os.path.join(experiment_dir, "metadata.csv"))
+# select only the columns that are needed
+d_metadata = d_metadata[["Instances", "Source"]]
+# Merge the metadata with the coordinates ("Row" = "Instances")
+d_coords = pd.merge(d_coords, d_metadata, left_on="Row", right_on="Instances")
+# Drop the "Instances" column
+d_coords.drop(columns="Instances", inplace=True)
 
 # If the source contains Evolution keep the str else fill with Original
 d_coords['Evolution'] = np.where(d_coords['Source'].str.contains('Evolution'), d_coords['Source'], 'Original')
@@ -165,40 +213,111 @@ d_svm = pd.read_csv(os.path.join(experiment_dir, "svm_table.csv"))
 
 # Read the bounds file
 d_bounds = pd.read_csv(os.path.join(experiment_dir, "bounds_prunned.csv"))
+if experiment in ["qaoa-param-inform-pub", "qaoa-classical-opts-init"]:
+    d_bounds = pd.read_csv(os.path.join(experiment_dir, "bounds.csv"))
+
 # Read the feature table
 d_features = pd.read_csv(os.path.join(experiment_dir, "feature_process.csv"))
 # Read the algorithm table
-d_algorithm = pd.read_csv(os.path.join(experiment_dir, "algorithm_raw.csv"))
-# Read the SVM table
+d_algorithm_raw = pd.read_csv(os.path.join(experiment_dir, "algorithm_raw.csv"))
+d_algorithm_process = pd.read_csv(os.path.join(experiment_dir, "algorithm_process.csv"))
+
+# Read the SVM table and best algorithm
 d_svm_preds = pd.read_csv(os.path.join(experiment_dir, "algorithm_svm.csv"))
+d_svm_selection = pd.read_csv(os.path.join(experiment_dir, "portfolio_svm.csv"))
+d_best_algo = pd.read_csv(os.path.join(experiment_dir, "portfolio.csv"))
+
+# Read the 
 
 
+mat = scipy.io.loadmat(os.path.join(experiment_dir, "model.mat"))
+algos = mat["data"]["algolabels"]
+algos = np.array([item for sublist in algos.flat for item in sublist.flat])
+
+# Update d_svm_selection "Best_Algorithm" column with the algorithm names
+d_svm_selection["Best_Algorithm"] = algos[d_svm_selection["Best_Algorithm"]-1]
+# Update d_best_algo "Best_Algorithm" column with the algorithm names
+d_best_algo["Best_Algorithm"] = algos[d_best_algo["Best_Algorithm"]-1]
+
+
+### DISPLAY THE DATA ###
 
 # Display the SVM table
 st.subheader("SVM")
-st.dataframe(d_svm[["Row","Probability_of_good", "CV_model_accuracy", "CV_model_precision"]])
 
+d_svm_display = d_svm[["Row","Probability_of_good", "CV_model_accuracy", "CV_model_precision"]].style.format({
+    "Probability_of_good": "{:.2%}",
+    # Just 2 d.p. for the accuracy and precision
+    "CV_model_accuracy": "{:.2f}",
+    "CV_model_precision": "{:.2f}"    
+})
+
+st.dataframe(d_svm_display)
 
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.subheader("Evolution")
-    fig = plot_evolution_chart(d_coords, d_bounds)
-    st.plotly_chart(fig, use_container_width=True)
+    # Make tabs for Source and Evolution
+    tabs = st.tabs(["Source Distribution", "Evolved Distribution"])
+    with tabs[0]:
+        st.subheader("Source Distribution")
+        fig = plot_evolution_chart(d_coords, d_bounds)
+        st.plotly_chart(fig, use_container_width=True)
+    
 
 
 with col2:
     st.subheader("Transformation")
     dataframe = pd.read_csv(f"data/external/ISA/{experiment}/projection_matrix.csv")
-    st.write(dataframe.T)  
+    dataframe = dataframe.T
+
+    # Filter first row
+    projection_matrix = dataframe.iloc[1:]
+    # Round all values to 3 decimal places
+    projection_matrix = projection_matrix.round(2).astype(str)
+    
+    projection_matrix = projection_matrix.to_latex(index=False, escape=False, column_format='', header=False). \
+        replace('\\begin{tabular}', '\\begin{bmatrix}').replace('\\end{tabular}', '\\end{bmatrix}'). \
+        replace('\\toprule', '').replace('\\midrule', '').replace('\\bottomrule', ''). \
+        replace('\\{c\\}', '')
+
+    # Extract index column
+    sel_feats = dataframe.reset_index()
+    # Remove first reow
+    sel_feats = sel_feats.iloc[1:]
+    # Select only the first column
+    sel_feats = sel_feats.iloc[:, 0]
+    # Add `\text{}` around each value
+    sel_feats = sel_feats.apply(lambda x: f'\\text{{{x}}}')
+    # Replace _ with space 
+    sel_feats = sel_feats.str.replace('_', ' ')
+    
+
+    # Convert to LaTeX
+    sel_feats = sel_feats.to_latex(index=False, escape=False, column_format='', header=False). \
+        replace('\\begin{tabular}', '\\begin{bmatrix}').replace('\\end{tabular}', '\\end{bmatrix}'). \
+        replace('\\toprule', '').replace('\\midrule', '').replace('\\bottomrule', ''). \
+        replace('\\{c\\}', '')
+
+
+    st.latex(
+        r"""
+
+            \begin{{bmatrix}}
+            Z_1 \\
+            Z_2
+            \end{{bmatrix}} = {projection_matrix}^\top
+            {sel_feats}
+        """.format(projection_matrix=projection_matrix, sel_feats=sel_feats)
+    )
         
 
 
 
 
 # Create 4 tabs
-tabs = st.tabs(["Feature Distribution", "Performance Distribution", "SVM Model Prediction", "SVM Selection"])
+tabs = st.tabs(["Feature Distribution", "Performance Distribution", "Best Algorithm", "SVM Model Prediction", "SVM Selection", "MATILDA Information"])
 
 # tabs = ["Feature Distribution", "Performance Distribution", "", "SVM Model Prediction", "SVM Selection"]
 
@@ -209,10 +328,43 @@ with tabs[0]:
 
 with tabs[1]:
     st.subheader("Performance Distribution")
+    # Add radio button to flick between raw and processed algorithm data
+    algorithm_data = st.radio("Algorithm Data", ["Raw", "Processed"])
+    if algorithm_data == "Raw":
+        d_algorithm = d_algorithm_raw
+    else:
+        d_algorithm = d_algorithm_process
+
     fig = plot_facetted_data(d_coords, d_algorithm)
     st.plotly_chart(fig, use_container_width=True)
 
-with tabs[2]:
-    st.subheader("SVM Model Prediction")
-    fig = plot_facetted_data(d_coords, d_svm_preds)
+    # Analyse the binary algorithm data
+    st.subheader("Binary Distribution")
+    d_algorithm_binary = pd.read_csv(os.path.join(experiment_dir, "algorithm_bin.csv"))
+    fig = plot_facetted_data(d_coords, d_algorithm_binary, binary_scale=True)
     st.plotly_chart(fig, use_container_width=True)
+
+with tabs[2]:
+    st.subheader("Best Algorithm")
+    fig = plot_facetted_data(d_coords, d_best_algo)
+    st.plotly_chart(fig, use_container_width=True)
+
+
+with tabs[3]:
+    st.subheader("SVM Model Prediction")
+    fig = plot_facetted_data(d_coords, d_svm_preds, binary_scale=True)
+    st.plotly_chart(fig, use_container_width=True)
+
+with tabs[4]:
+    st.subheader("SVM Selection")
+    fig = plot_facetted_data(d_coords, d_svm_selection)
+    st.plotly_chart(fig, use_container_width=True)
+
+with tabs[5]:
+    st.subheader("Model Information")
+    # Read options.json
+    options = pd.read_json(os.path.join(experiment_dir, "options.json"), typ='series')
+    # Fully unnest the options
+    st.write(options)
+
+    
